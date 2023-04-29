f572e7f7ed254f6efdc8ec34453a5e46d6a876f3
Galyaminsky-Notebook <jobforandroid@proton.me>
2023-04-29 13:26:16 +0600

    ShopListRepositoryImpl

---
+++ app/src/main/java/me/proton/jobforandroid/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package me.proton.jobforandroid.data
+
+import me.proton.jobforandroid.domain.ShopItem
+import me.proton.jobforandroid.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository{
+
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId } ?: throw IllegalArgumentException("No shop item with id $shopItemId")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+}
--- app/src/main/java/me/proton/jobforandroid/domain/ShopItem.kt
+++ app/src/main/java/me/proton/jobforandroid/domain/ShopItem.kt
@@ -1,9 +1,15 @@
 package me.proton.jobforandroid.domain
 
 data class ShopItem(
-    val id: Int,
+
     val name: String,
     val count: Int,
     val enabled: Boolean,
+    var id: Int = UNDEFINED_ID,
 
-    )
+    ) {
+
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
